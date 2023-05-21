4816e170c6459d801d367c3266542288db29e4b3
Adam Kulik <92215997+Adashevski@users.noreply.github.com>
2023-05-21 13:04:27 +0200

    update
    -zmiana nazwy funkcji(renderMoviesList)
    -export funkcji i wywołanie w pliku index.js
    -utworzenie folderu sass/partials
    -inne zmiany kosmetyczne

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,6 @@
 export const API_KEY = 'dd28573eee75132a5c1e09d16e28974f';
 export const API_URL = 'https://api.themoviedb.org/3/';
 export const API_LANGUAGE = 'en-US';
+
+// API URL's
+export const TRENDING_URL = `${API_URL}trending/movie/week?api_key=${API_KEY}`;
