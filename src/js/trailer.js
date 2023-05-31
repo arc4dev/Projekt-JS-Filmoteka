7947cc05169e7fa910878b6a89f896012f0b6514
Adrian Wojtczak <117528537+adrianwojtczak@users.noreply.github.com>
2023-05-31 11:43:22 +0200

    New trailer get method

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -12,32 +12,72 @@ const closeModalHandler = (ev) => {
   }
 };
 
+// trailerBtn.addEventListener('click', async () => {
+//   const movieId = trailerBtn.dataset.trailer;
+
+//   const url = `${API_URL}movie/${movieId}/videos?api_key=${API_KEY}`;
+
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     console.log(data.results);
+//     console.log(data.results.length);
+
+//     if (data.results.length === 0) {
+//       throw new Error(
+//         'We apologize, but there is no trailer available for this movie.'
+//       );
+//     }
+
+//     const trailerKey = data.results[0].key;
+
+// const content = `<iframe width="800" height="450" src="https://www.youtube.com/embed/${trailerKey}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+
+// lightboxInstance = basicLightbox.create(content);
+// lightboxInstance.show();
+
+// window.addEventListener('keydown', closeModalHandler);
+//   } catch (error) {
+//     Notify.failure(
+//       'We apologize for the inconvenience, but an unexpected error has occurred.'
+//     );
+//   }
+// });
+
 trailerBtn.addEventListener('click', async () => {
   const movieId = trailerBtn.dataset.trailer;
 
-  const url = `${API_URL}movie/${movieId}/videos?api_key=${API_KEY}`;
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+    },
+  };
+
+  const url = `${API_URL}movie/${movieId}/videos?language=en-US&api_key=${API_KEY}`;
 
   try {
-    const response = await fetch(url);
+    const response = await fetch(url, options);
     const data = await response.json();
 
-    if (data.results.length === 0) {
-      throw new Error(
+    if (data.results.length > 0) {
+      const trailerKey = data.results[0].key;
+
+      const content = `<iframe width="800" height="450" src="https://www.youtube.com/embed/${trailerKey}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+
+      lightboxInstance = basicLightbox.create(content);
+      lightboxInstance.show();
+
+      window.addEventListener('keydown', closeModalHandler);
+    } else {
+      Notify.warning(
         'We apologize, but there is no trailer available for this movie.'
       );
     }
-
-    const trailerKey = data.results[0].key;
-
-    const content = `<iframe width="800" height="450" src="https://www.youtube.com/embed/${trailerKey}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-
-    lightboxInstance = basicLightbox.create(content);
-    lightboxInstance.show();
-
-    window.addEventListener('keydown', closeModalHandler);
   } catch (error) {
     Notify.failure(
       'We apologize for the inconvenience, but an unexpected error has occurred.'
     );
+    console.error(error);
   }
 });
